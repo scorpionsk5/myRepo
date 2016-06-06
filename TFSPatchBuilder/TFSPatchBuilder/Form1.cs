03eea065c06b67c92ef8060ea08ee79e81ed1118
scorpion.sk5 <sashikumar148@gmail.com>
2016-06-06 23:20:33 +0530

    new project

---
+++ TFSPatchBuilder/TFSPatchBuilder/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework.Common;
+using Microsoft.TeamFoundation.Framework.Client;
+
+namespace TFSPatchBuilder
+{
+    public partial class TFSPatchBuilder : Form
+    {
+        public TFSPatchBuilder()
+        {
+            InitializeComponent();
+        }
+
+        public void ConnectToTFSServer(string ServerURL)
+        {
+            // Connect to Team Foundation Server
+            //     Server is the name of the server that is running the application tier for Team Foundation.
+            //     Port is the port that Team Foundation uses. The default port is 8080.
+            //     VDir is the virtual path to the Team Foundation application. The default path is tfs.
+            Uri tfsUri = new Uri(ServerURL);
+            TfsConfigurationServer configurationServer =
+                    TfsConfigurationServerFactory.GetConfigurationServer(tfsUri);
+
+            // Get the catalog of team project collections
+            ReadOnlyCollection<CatalogNode> collectionNodes = configurationServer.CatalogNode.QueryChildren(
+                new[] { CatalogResourceTypes.ProjectCollection },
+                false, CatalogQueryOptions.None);
+
+            // List the team project collections
+            foreach (CatalogNode collectionNode in collectionNodes)
+            {
+                // Use the InstanceId property to get the team project collection
+                Guid collectionId = new Guid(collectionNode.Resource.Properties["InstanceId"]);
+                TfsTeamProjectCollection teamProjectCollection = configurationServer.GetTeamProjectCollection(collectionId);
+
+                // Print the name of the team project collection
+                Console.WriteLine("Collection: " + teamProjectCollection.Name);
+
+                // Get a catalog of team projects for the collection
+                ReadOnlyCollection<CatalogNode> projectNodes = collectionNode.QueryChildren(
+                    new[] { CatalogResourceTypes.TeamProject },
+                    false, CatalogQueryOptions.None);
+            }
+        }
+
+    }
+}
