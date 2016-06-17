59bbeee407d50e23aa5264f97fd80b4d0577abb6
Shashikumar D <scorpion.sk5@hotmail.com>
2016-06-17 15:42:52 +0530

    update

---
+++ API Manager App/API Manager/js/_references.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="~/js/lib/kendo.all-vsdoc.js" />
+/// <reference path="~/js/lib/require.js" />
+
+requirejs.config({
+    baseUrl: '~/js',
+    paths: {
+        jquery: 'lib/jquery-2.2.2.min',
+        kendo: 'lib/kendo.all.min',
+        text: 'lib/text',
+        underscore: 'lib/underscore-min',
+        nicescroll: 'lib/nicescroll'
+    }
+});
