bd51bf79759d7119ac221fa25d1f438528825cae
Shashikumar D <scorpion.sk5@hotmail.com>
2016-06-20 15:40:02 +0530

    update

--- API Manager App/API Manager/js/_references.js
+++ API Manager App/API Manager/js/_references.js
@@ -1,5 +1,2 @@
 ﻿/// <reference path="~/js/lib/require.js" />
-/// <reference path="~/js/lib/jquery-2.2.2.min.js" />
-/// <reference path="~/js/lib/kendo.all.min.js" />
-/// <reference path="~/js/lib/underscore-min.js" />
 
