a7c7017ae2d88c91f13390b1378c4287394b0697
Shashikumar D <scorpion.sk5@hotmail.com>
2016-06-20 15:02:44 +0530

    update

--- API Manager App/API Manager/js/app.js
+++ API Manager App/API Manager/js/app.js
@@ -3,10 +3,10 @@
     requirejs.config({
         baseUrl: 'js',
         paths: {
-            jquery: 'lib/jquery',
-            kendo: 'lib/kendo',
+            jquery: 'lib/jquery-2.2.2.min',
+            kendo: 'lib/kendo.all.min',
             text: 'lib/text',
-            underscore: 'lib/underscore',
+            underscore: 'lib/underscore-min',
             nicescroll: 'lib/nicescroll'
         },
         waitSeconds: 0
