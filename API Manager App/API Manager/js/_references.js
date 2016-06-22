5077c710559643539e63052c117ec493d90657ea
Shashikumar D <scorpion.sk5@hotmail.com>
2016-06-22 18:37:52 +0530

    update

--- API Manager App/API Manager/js/_references.js
+++ API Manager App/API Manager/js/_references.js
@@ -1,12 +1,6 @@
-﻿/// <reference path="~/js/lib/require.js" />
-
-requirejs.config({
-    baseUrl: '~/js',
-    paths: {
-        jquery: 'lib/jquery-2.2.2.min',
-        kendo: 'lib/kendo.all.min',
-        text: 'lib/text',
-        underscore: 'lib/underscore-min',
-        nicescroll: 'lib/nicescroll'
-    }
-});
+﻿/// <reference path="lib/require.js" />
+/// <reference path="lib/jquery-2.2.2.min.js" />
+/// <reference path="lib/underscore-min.js" />
+/// <reference path="lib/kendo.all.min.js" />
+/// <reference path="lib/nicescroll.js" />
+/// <reference path="app/APIManager.js" />
