fb905ed66dfc6eda70b171bd6ba9ee42cffd6eee
scorpionsk5 <sashikumar148@gmail.com>
2016-08-23 00:29:15 +0530

    update

--- UnitTestBuilder/UnitTestBuilder/js/app/view/view.js
+++ UnitTestBuilder/UnitTestBuilder/js/app/view/view.js
@@ -8,9 +8,14 @@
                 properties.cssClass && newElement.addClass(properties.cssClass);
                 properties.style && newElement.css(properties.style);
                 properties.text && newElement.text(properties.text);
+                properties.data && newElement.data(properties.data);
             };
 
             return newElement;
+        },
+
+        constructMenuItem: function (text, dataAction) {
+            return this.createElement('span', { cssClass: 'MenuItem', text: text, data: { action: dataAction } });
         }
     },
 
@@ -18,16 +23,22 @@
             init: function ($container) {
                 this.$container = $container;
                 var $mainContainer = utils.createElement('div', { cssClass: 'MainContainer' }),
-                    $sidebarMenu = utils.createElement('aside', { cssClass: 'SidebarMenu' }),
                     $caseBuilderContainer = utils.createElement('div', { cssClass: 'CaseBuilderContainer' });
 
-                $mainContainer.append([utils.createElement('h2', { text: 'Test Builder', cssClass: 'Headers' }), $sidebarMenu, $caseBuilderContainer]);
+                $mainContainer.append([utils.createElement('div', { cssClass: 'MainMenu' }), $caseBuilderContainer]);
                 $mainContainer.appendTo(this.$container);
+
+                this.$mainContainer = $mainContainer;
+                this.$caseBuilderContainer = $caseBuilderContainer;
             },
 
             postInit: function (appInstance) {
                 this.app = appInstance;
-                this.addCase();
+                this.buildMenu();
+            },
+
+            buildMenu: function () {
+                this.$mainContainer.find('.MainMenu').append([utils.constructMenuItem('Home', 'Home'), utils.constructMenuItem('About', 'About'), utils.constructMenuItem('Help', 'Help')]);
             },
 
             addCase: function () {
@@ -35,7 +46,9 @@
             },
 
             _buildCaseView: function () {
-                var $caseView = utils.createElement('div', { cssClass: 'CaseView' });
+                var $caseView = utils.createElement('div', { cssClass: 'CaseView' }),
+                    $caseName = utils.createElement('input', { cssClass: 'k-textbox k-input' });
+
             }
         });
 
