85843ac672fcec91d99a6cdc7356c90bf9a32df4
Shashikumar D <scorpion.sk5@hotmail.com>
2016-09-29 18:40:33 +0530

    removed APIManager project

--- API Manager App/API Manager/js/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="lib/require.js" />
-/// <reference path="lib/jquery-2.2.2.min.js" />
-/// <reference path="lib/underscore-min.js" />
-/// <reference path="lib/kendo.all.min.js" />
-/// <reference path="lib/nicescroll.js" />
-/// <reference path="app/APIManager.js" />
